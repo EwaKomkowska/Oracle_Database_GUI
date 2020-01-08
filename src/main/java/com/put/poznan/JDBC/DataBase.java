d5a093463d5a124b5a805fd7cc37646335190d85
NareszcieCosSieDzieje <48532131+NareszcieCosSieDzieje@users.noreply.github.com>
2020-01-08 21:38:13 +0100

    -add DataBase class-

---
+++ src/main/java/com/put/poznan/JDBC/DataBase.java
@@ -0,0 +1,38 @@
+package com.put.poznan.JDBC;
+
+import java.sql.*;
+
+public class DataBase {
+
+    private Connection connection;
+
+    public DataBase(){
+
+    }
+
+    public DataBase(String login, String password){
+        this.setUp(login,password);
+    }
+
+    public void setUp(String login, String password){
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            this.connection = DriverManager.getConnection( "jdbc:oracle:thin:@localhost:1521:PAWEL",login, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void closeConnection(){
+        try {
+            this.connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.put.poznan {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens com.put.poznan to javafx.fxml;
     exports com.put.poznan;
